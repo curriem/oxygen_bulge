8fcc92b3f27a5dda5524690dd26ff9ee3d1e269c
Miles Currie <mcurr@D-10-19-233-71.dhcp4.washington.edu>
2019-05-08 10:13:40 -0700

    updates

--- scripts/make_run_scripts.py
+++ scripts/make_run_scripts.py
@@ -1,3 +1,5 @@
+import project_tools
+
 o2_bulge_options = ['upper', 'lower', 'mixed']
 
 o2inv_options = ['0.21']
@@ -6,20 +8,20 @@ n2_options = ['1.', '0.17', '0.007']
 run_type_options = ['bands', 'full']
 
 # upper o2 case
-
+runfiles = []
 for o2_bulge in o2_bulge_options:
+    for run_type in run_type_options:
+        for o2inv in o2inv_options:
+            for pt_shape in pt_shape_options:
+                for n2 in n2_options:
+                    runfile = 'run_experiment.py -o2_loc %s -o2inv %s -n2scale %s -pt_shape %s -run_type %s' % (o2_bulge,
+                                                                                                                       o2inv,
+                                                                                                                       n2,
+                                                                                                                       pt_shape,
+                                                                                                                       run_type)
+                    runfiles.append(runfile)
 
-    with open('./job_%s.sh' % o2_bulge, 'wb') as f:
-        for run_type in run_type_options:
-            for o2inv in o2inv_options:
-                for pt_shape in pt_shape_options:
-                    for n2 in n2_options:
-                        f.write('python ')
-                        f.write('/Users/mcurr/RESEARCH/oxygen_bulge/scripts/')
-                        f.write('run_experiment.py')
-                        f.write(' -o2_loc %s ' % o2_bulge)
-                        f.write(' -o2inv %s ' % o2inv)
-                        f.write(' -n2scale %s ' % n2)
-                        f.write(' -pt_shape %s ' % pt_shape)
-                        f.write(' -run_type %s ' % run_type)
-                        f.write('\n')
+project_tools.write_slurm_script_python(runfiles,
+                                        name='o2bulge',
+                                        subname='o2bulge.sh',
+                                        workdir='./')
