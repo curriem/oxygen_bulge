43cb13d36809028c91639456ab0637f75191ccac
M. Currie <mcurr@mox1.hyak.local>
2019-05-16 16:50:25 -0700

    hyak changes

--- scripts/make_pt_files.py
+++ scripts/make_pt_files.py
@@ -1,7 +1,7 @@
 import project_tools
 import numpy as np
-o2_abundances = np.arange(0.05, 0.45, 0.05)
-locs = ['upper', 'mixed']
+o2_abundances = np.arange(0.05, 0.08, 0.001)
+locs = ['mixed']
 for o2_abundance in o2_abundances:
     for loc in locs:
         project_tools.make_atmosphere(loc, o2_abundance)
--- scripts/make_run_scripts.py
+++ scripts/make_run_scripts.py
@@ -4,21 +4,35 @@ import glob
 import sys
 
 name = sys.argv[1]
+star = sys.argv[2]
+planet = sys.argv[3]
 
-pt_files = glob.glob('../data/pt_fls/*')
-locs = ['upper', 'lower']
+pt_files1 = glob.glob('../data/pt_fls/*_[1,2,3,4,5]*')
+pt_files2 = glob.glob('../data/pt_fls/*_01*')
+pt_files3 = glob.glob('../data/pt_fls/*_02*')
+pt_files4 = glob.glob('../data/pt_fls/*_03*')
+pt_files5 = glob.glob('../data/pt_fls/*_04*')
+pt_files6 = glob.glob('../data/pt_fls/*_05*')
+pt_files7 = glob.glob('../data/pt_fls/*_06*')
+pt_files8 = glob.glob('../data/pt_fls/*_07*')
+pt_files9 = glob.glob('../data/pt_fls/*_08*')
 
-star = 'trappist1'
-planet = 't1e'
+new = glob.glob('../data/pt_fls/*_09*') +  glob.glob('../data/pt_fls/*_1*')
 
-runfiles = []
-for pt_file in pt_files:
-    runfile = 'run_experiment.py -star %s -planet %s -pt_fl %s -wlmin 0.04 -wlmax 1.5' % (star, planet, pt_file)
-    runfiles.append(runfile)
+pt_files_master = [pt_files9, pt_files8, pt_files7, pt_files6]#, pt_files5, pt_files6]
+
+for n in range(4):
+        pt_files = pt_files_master[n]
+	runfiles = []
+	for pt_file in pt_files:
+	    pt_file = pt_file.split('/')[-1]
+	    print pt_file
+	    runfile = 'run_experiment.py -star %s -planet %s -pt_fl %s -wlmin 0.04 -wlmax 1.5' % (star, planet, pt_file)
+	    runfiles.append(runfile)
 
 
-project_tools.write_slurm_script_python(runfiles,
-                                        name=name,
-                                        subname=name+'.sh',
-                                        workdir='./',
-					walltime='100:00:00')
+	project_tools.write_slurm_script_python(runfiles,
+						name=name+str(n+1),
+						subname=name+str(n+1)+'.sh',
+						workdir='./',
+						walltime='100:00:00')
