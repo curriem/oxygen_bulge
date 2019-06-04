0f4e94f323f34ee1f9b2b2486077af0dcf66d67c
Miles Currie <mcurr@D-10-19-206-87.dhcp4.washington.edu>
2019-06-04 09:48:24 -0700

    cold trap variable

--- scripts/make_pt_files.py
+++ scripts/make_pt_files.py
@@ -1,7 +1,7 @@
 import project_tools
 import numpy as np
-o2_abundances = np.arange(0.001, 0.01, 0.001)
-locs = ['mixed']
-for o2_abundance in o2_abundances:
-    for loc in locs:
-        project_tools.make_atmosphere(loc, o2_abundance)
+o2_abundance = 0.2
+loc = 'upper'
+cold_traps = np.logspace(-3, 0, 30)
+for cold_trap in cold_traps:
+    project_tools.make_atmosphere(loc, o2_abundance, cold_trap)
